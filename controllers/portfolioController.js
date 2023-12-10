d230a956cc6453d34959fd265026107128239865
NeelTilak <neeltilak2.1998@gmail.com>
2023-12-10 20:08:50 +0530

    chnages in controller
    debug the api integrations
    
    10/12/23

--- controllers/portfolioController.js
+++ controllers/portfolioController.js
@@ -1,44 +1,48 @@
-const nodemailer = require("nodemailer");
-const sendGridTransport = require("nodemailer-sendgrid-transport");
+const nodemailer = require('nodemailer')
+const sendGridTransport = require('nodemailer-sendgrid-transport')
 
-//transport
+
 const transporter = nodemailer.createTransport(
   sendGridTransport({
-    auth: {
-      api_key: process.env.API_SENDGRID,
-    },
-  })
-);
-
-const sendEmailController = (req, res) => {
-  try {
-    const { name, email, msg } = req.body;
-
-    //validation
-    if (!name || !email || !msg) {
-      return res.status(500).send({
-        success: false,
-        message: "Please Provide All Fields",
-      });
+    auth : {
+      api_key: process.env.API_SENDGRID
     }
-    //email matter
+  })
+)
+
+
+const  sendEmailController = (req, res) => {
+  try {
+    const [name,email,msg] = req.body
+
+    if(!name || !email || !msg){
+      return res.status(500).send({
+        success:false,
+        message:'please provide all fields'
+      })
+    }
+
     transporter.sendMail({
-      to: "rktechnicalplus@gmail.com",
-      from: "rktechnicalplus@gmail.com",
-      subject: "Regarding Mern Portfolio App",
-      html: `
-        <h5>Detail Information</h5>
-        <ul>
-          <li><p>Name : ${name}</p></li>
-          <li><p>Email : ${email}</p></li>
-          <li><p>Message : ${msg}</p></li>
-        </ul>
-      `,
-    });
+      to: 'neeltilak3.1998@gmail.com',
+      from: 'neeltilak.1998@gmail.com',
+      subject: 'Regards of mern',
+      html: 
+      `
+      <h5>
+      Detail Information
+      </h5>
+      <ul>
+      <li><p>Name : ${name}</p></li>
+      <li><p>Email : ${email}</p></li>
+      <li><p>Message : ${msg}</p></li>
+      </ul>
+      `
+
+    })
 
     return res.status(200).send({
-      success: true,
-      message: "Your Message Send Successfully",
+        success:true,
+        message:'Your Message Send Successfully'
     });
   } catch (error) {
     console.log(error);
